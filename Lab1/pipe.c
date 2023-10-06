7c83cb45ae79b09df34917ff9ca2e8aeb1168db2
Arthur Zhou <arthuracct@gmail.com>
2023-10-06 13:49:27 -0700

    forgot includes

--- Lab1/pipe.c
+++ Lab1/pipe.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 int main(int argc, char *argv[]) {
     for (int i = 0; i < argc; i++) {
         printf("argument %d is %s\n", i, argv[i]);
