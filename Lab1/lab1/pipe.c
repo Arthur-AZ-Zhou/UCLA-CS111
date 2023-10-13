e109d40b84162130b7aaf6af29f266c8232a31f5
Arthur Zhou <arthuracct@gmail.com>
2023-10-13 14:32:21 -0700

    bugfix

--- Lab1/lab1/pipe.c
+++ Lab1/lab1/pipe.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]) {
         printf("argument %d is %s\n", i, argv[i]);
     }
 
-	int returnCode = system(argv[0]);
+	int returnCode = system(argv[1]);
 
 	if (returnCode == 0) {
 		printf("Command executed successfully.\n");
@@ -17,8 +17,8 @@ int main(int argc, char *argv[]) {
 		printf("Command failed w/ return code %d.\n", returnCode);
 	}
 
-	// printf("All files in executable path: ");
-	// execlp("ls", "ls", NULL); //lists out all files in ls' executable path
+	printf("All files in executable path: ");
+	execlp("ls", "ls", NULL); //lists out all files in ls' executable path
 
     return 0;
 }
