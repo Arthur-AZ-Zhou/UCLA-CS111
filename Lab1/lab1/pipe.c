9c55a6d6ed8e56b77e8106b81689d6774676a5ac
Arthur Zhou <arthuracct@gmail.com>
2023-10-13 12:02:35 -0700

    removed packed MAC files

--- Lab1/lab1/pipe.c
+++ Lab1/lab1/pipe.c
@@ -4,8 +4,12 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(int argc, char *argv[])
-{
-	// TODO: it's all yours
-	return 0;
+int main(int argc, char *argv[]) {
+    for (int i = 0; i < argc; i++) {
+        printf("argument %d is %s\n", i, argv[i]);
+    }
+
+    execlp("ls", "ls", NULL);
+
+    return 0;
 }
