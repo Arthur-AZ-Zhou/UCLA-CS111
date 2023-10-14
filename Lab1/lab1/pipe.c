2c4375461b2df04dd91efcbff3b4e25923ecfdb0
Arthur Zhou <arthuracct@gmail.com>
2023-10-13 18:29:48 -0700

    attempt 1

--- Lab1/lab1/pipe.c
+++ Lab1/lab1/pipe.c
@@ -5,17 +5,38 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
-    for (int i = 0; i < argc; i++) {
-        printf("argument %d is %s\n", i, argv[i]);
-    }
+    // for (int i = 0; i < argc; i++) {
+    //     printf("argument %d is %s\n", i, argv[i]);
+    // }
 
-	int returnCode = system(argv[1]);
+	int i;
 
-	if (returnCode == 0) {
-		printf("Command executed successfully.\n");
-	} else {
-		printf("Command failed w/ return code %d.\n", returnCode);
+	for (i = 1; i < argc - 1; i++) {
+		int fd[2];
+		pipe(fd);
+
+		if (!fork()) {
+			dup2(pd[1], 1); //link stdout to parent
+			execlp(argv[i], argv[i], NULL);
+			perror("exec");
+			abort();
+		}
+
+		dup2(pd[0], 0);
+		close(pd[1]);
 	}
+
+	execlp(argv[i], argv[i], NULL);
+	perror("exec");
+	abort();
+
+	// int returnCode = system(argv[1]);
+
+	// if (returnCode == 0) {
+	// 	printf("Command executed successfully.\n");
+	// } else {
+	// 	printf("Command failed w/ return code %d.\n", returnCode);
+	// }
 
 	// printf("All files in executable path: ");
 	// execlp("ls", "ls", NULL); //lists out all files in ls' executable path
